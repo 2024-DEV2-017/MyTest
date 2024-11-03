1ac86f36956f1283a73e62d5c3c63fdbfc057b9b
Anon <anon@foobar.com>
2024-11-03 16:07:20 +0100

    feat(GameViewModel): change logic of 'gamestate' in favor of 'gameActive' and 'winner'

--- app/src/main/java/com/example/tictactoe/viewmodels/GameViewModel.kt
+++ app/src/main/java/com/example/tictactoe/viewmodels/GameViewModel.kt
@@ -10,13 +10,14 @@ data class Cell(val player: Player? = null)
 class GameViewModel : ViewModel() {
     var board = mutableStateOf(List(3) { List(3) { Cell() } }) // Assume the gameboard is always 3x3
     var currentPlayer = mutableStateOf(Player.X) // Player X starts the game
-    var gameStatus = mutableStateOf("")
+    var winner = mutableStateOf<Player?>(null)
+    var gameActive = mutableStateOf(true)
 
     var onWin: ((Player)) -> Unit? = {}
     var onDraw: () -> Unit? = {}
 
     fun makeMove(row: Int, col: Int) {
-        if (board.value[row][col].player == null && gameStatus.value.isEmpty()) {
+        if (board.value[row][col].player == null && gameActive.value) {
             board.value = board.value.mapIndexed { r, rowList ->
                 rowList.mapIndexed { c, cell ->
                     if (r == row && c == col) Cell(currentPlayer.value) else cell
@@ -24,8 +25,11 @@ class GameViewModel : ViewModel() {
             }
             if (checkWin()) {
                 onWin(currentPlayer.value)
+                winner.value = currentPlayer.value
+                gameActive.value = false
             } else if (board.value.flatten().all { it.player != null }) {
                 onDraw()
+                gameActive.value = false
             } else { // swap active player
                 currentPlayer.value = if (currentPlayer.value == Player.X) Player.O else Player.X
             }
@@ -33,7 +37,7 @@ class GameViewModel : ViewModel() {
     }
 
     private fun checkWin(): Boolean {
-        // Assuming the board is always 3x3, there are only so many possible win conditions
+        // Assuming the board is always 3x3, there are only 8 possible win conditions
         val lines = listOf(
             // Rows
             listOf(board.value[0][0], board.value[0][1], board.value[0][2]),
